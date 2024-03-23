f94d6db854915f8a523939b15710765064b5e4d3
SiddhiHate13 <siddhihaate@gmail.com>
2024-03-23 22:49:09 +0530

    reactapp

---
+++ src/App1.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class App1 extends Component {
+  render() {
+    return (
+      <div>
+              </div>
+    )
+  }
+}
+
+export default App1
